047128708ed1467e9fbcfbea1889f82d7475a57d
Johannes <johannes.postma@hotmail.com>
2021-02-12 21:37:35 +0100

    updated

--- src/main/java/com/johannes/demo/MyController.java
+++ src/main/java/com/johannes/demo/MyController.java
@@ -16,7 +16,7 @@ public class MyController {
     public Map hello() {
         log.info("hello johannes");
         log.debug("hello liesbeth");
-        return Collections.singletonMap("message", "hello springboot from BBB");
+        return Collections.singletonMap("message", "hello springboot from CCC");
     }
 
     @ExceptionHandler(Exception.class)

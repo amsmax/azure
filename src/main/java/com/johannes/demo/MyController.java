37359fb9bfb4ab6673f234e0d82a02df28618323
Johannes <johannes.postma@hotmail.com>
2021-02-12 20:11:17 +0100

    Changed message

--- src/main/java/com/johannes/demo/MyController.java
+++ src/main/java/com/johannes/demo/MyController.java
@@ -16,7 +16,7 @@ public class MyController {
     public Map hello() {
         log.info("hello johannes");
         log.debug("hello liesbeth");
-        return Collections.singletonMap("message", "hello springboot from AAA");
+        return Collections.singletonMap("message", "hello springboot from BBB");
     }
 
     @ExceptionHandler(Exception.class)
